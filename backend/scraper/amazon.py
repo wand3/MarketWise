58fab7786068064e6005f10b1024c5e31601a68d
wand3 <godswillbaiyewu@gmail.com>
2025-04-13 20:09:54 +0100

    extract product image success

--- backend/scraper/amazon.py
+++ backend/scraper/amazon.py
@@ -10,18 +10,34 @@ from logger import logger
 from asyncio import gather
 
 
-def get_stock(driver):
-    # Get the page content
-    html_code = driver.page_source
-
-    # get all searched products on each page scroll
-    soup = BeautifulSoup(html_code, 'html.parser')
-
+def get_stock(soup):
     # Find all divs with the specified class
     all_divs = soup.find_all("div", attrs={"role": "listitem"})
     logger.info(f"divs on scroll: {len(all_divs)}")
 
     return all_divs
+
+
+def get_product(driver):
+    # Get the page content
+    html_code = driver.page_source
+    # get all searched products on each page scroll
+    soup = BeautifulSoup(html_code, 'html.parser')
+    listitems = get_stock(soup)
+    # logger.info(listitems)
+
+    product_name_element = ''
+    product_price_element = ''
+    product_link = ''
+
+    products = []
+    for item in listitems:
+        product_image = item.find("img", class_="s-image")
+        if product_image:
+            href = product_image.get('href')
+        logger.info(product_image)
+        products.append(product_image)
+    return products
 
 
 # scroll to pagination part of page
@@ -49,7 +65,9 @@ def scroll_page(driver):
         EC.element_to_be_clickable((By.PARTIAL_LINK_TEXT, "Next"))
     )
     # get all items using bs4
-    items = get_stock(driver)
+    # items = get_stock(driver)
+    items = get_product(driver)
+
     logger.info(items)
 
     if next_page:
@@ -65,39 +83,38 @@ def scroll_page(driver):
     logger.info("Scroll to bottom success")
 
 
-
 #     elements = await product_div.query_selector_all('.a-size-base')
 #     filtered_elements = [element for element in elements if 'stock' in await element.inner_text()]
 #     return filtered_elements
 
 
-async def get_product(product_div):
-    # Query for all elements at once
-    image_element_future = product_div.query_selector('img.s-image')
-    name_element_future = product_div.query_selector(
-        'h2 a span')
-    price_element_future = product_div.query_selector('span.a-offscreen')
-    url_element_future = product_div.query_selector(
-        'a.a-link-normal.s-no-hover.s-underline-text.s-underline-link-text.s-link-style.a-text-normal')
-
-    # Await all queries at once
-    image_element, name_element, price_element, url_element = await gather(
-        image_element_future,
-        name_element_future,
-        price_element_future,
-        url_element_future,
-        # get_stock(product_div)
-    )
-
-    # Fetch all attributes and text at once
-    image_url = await image_element.get_attribute('src') if image_element else None
-    product_name = await name_element.inner_text() if name_element else None
-    try:
-        print((await price_element.inner_text()).replace("$", "").replace(",", "").strip())
-        product_price = float((await price_element.inner_text()).replace("$", "").replace(",", "").strip()) if price_element else None
-    except:
-        product_price = None
-    product_url = "/".join((await url_element.get_attribute('href')).split("/")[:4]) if url_element else None
-    # stock = stock_element[0] if len(stock_element) > 0 else None
-
-    return {"img": image_url, "name": product_name, "price": product_price, "url": product_url}
+# async def get_product(product_div):
+#     # Query for all elements at once
+#     image_element_future = product_div.query_selector('img.s-image')
+#     name_element_future = product_div.query_selector(
+#         'h2 a span')
+#     price_element_future = product_div.query_selector('span.a-offscreen')
+#     url_element_future = product_div.query_selector(
+#         'a.a-link-normal.s-no-hover.s-underline-text.s-underline-link-text.s-link-style.a-text-normal')
+#
+#     # Await all queries at once
+#     image_element, name_element, price_element, url_element = await gather(
+#         image_element_future,
+#         name_element_future,
+#         price_element_future,
+#         url_element_future,
+#         # get_stock(product_div)
+#     )
+#
+#     # Fetch all attributes and text at once
+#     image_url = await image_element.get_attribute('src') if image_element else None
+#     product_name = await name_element.inner_text() if name_element else None
+#     try:
+#         print((await price_element.inner_text()).replace("$", "").replace(",", "").strip())
+#         product_price = float((await price_element.inner_text()).replace("$", "").replace(",", "").strip()) if price_element else None
+#     except:
+#         product_price = None
+#     product_url = "/".join((await url_element.get_attribute('href')).split("/")[:4]) if url_element else None
+#     # stock = stock_element[0] if len(stock_element) > 0 else None
+#
+#     return {"img": image_url, "name": product_name, "price": product_price, "url": product_url}
