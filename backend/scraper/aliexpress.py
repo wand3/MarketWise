0837ce37e145bd3653a37752ec2aa0d0729594ed
wand3 <godswillbaiyewu@gmail.com>
2025-06-10 08:52:32 +0100

    aliexpress scrapper product search

--- backend/scraper/aliexpress.py
+++ backend/scraper/aliexpress.py
@@ -15,7 +15,7 @@ import json
 
 logger = setup_logger("aliexpress", "DEBUG", "scraper.log")
 # uniform to mimic more human like randon with floats
-delay = random.uniform(5, 15)
+delay = random.uniform(3, 6)
 
 ALIEXPRESS = "https://aliexpress.com"
 URLS = {
@@ -145,91 +145,81 @@ def get_product(driver):
 # scroll to pagination part of page
 def scroll_page_aliexpress(driver):
     logger.info("Scroll in")
-
-    # Wait for the search button to appear and click it
-    # dismiss_button = WebDriverWait(driver, 5).until(
-    #     EC.element_to_be_clickable((By.CLASS_NAME, 'esm--upload-close--1x0SREz'))
-    # )
-    # logger.info("dismiss success")
-    # dismiss_button = driver.find_element(By.CLASS_NAME, 'esm--upload-close--1x0SREz')
-    # if dismiss_button:
-    #     dismiss_button.click()
-    #     logger.info("click success")
-    # logger.info("Dismiss didn't pop up")
-    scroll_count = 15
-    height = 0
-
-    for i in range(scroll_count):
-        driver.execute_script(f"window.scrollBy({height}, 600);")  # Scrolls down 500px
-        height += (height + (i * height))
-        # Pause for a random time between 5 and 15 seconds
-        time.sleep(3)
-        # Scroll to pagination
-        # go_to_page = driver.find_element(By.XPATH, "//div[contains(@class,'comet-pagination-options-quick-jumper')]")
-        # go_to_page = driver.find_element(By.CSS_SELECTOR, "li.comet-pagination-next > button.comet-pagination-item-link")
-        # go_to_page = driver.find_element(
-        #     By.XPATH,
-        #     "//li[contains(@class,'comet-pagination-next') and @aria-disabled='false']/button"
-        # )
-
+    page_visited = 0
+    while True:
         try:
-            go_to_page = driver.find_element(By.CLASS_NAME, "comet-pagination-item-link")
+            # Wait for the dismiss button to appear and click it
+            dismiss_button = WebDriverWait(driver, 8).until(
+                EC.element_to_be_clickable((By.CLASS_NAME, 'esm--upload-close--1x0SREz'))
+            )
+            logger.info("dismiss success")
+            dismiss_button = driver.find_element(By.CLASS_NAME, 'esm--upload-close--1x0SREz')
+            if dismiss_button:
+                dismiss_button.click()
+                logger.info("click success")
+        except Exception as e:
+            logger.info("Dismiss didn't pop up")
+            pass
+        finally:
+            scroll_count = 15
+            height = 0
 
-            if go_to_page:
+            for i in range(scroll_count):
                 try:
-                    items = get_product(driver)
-                    logger.error(f"Items on Scroll {items}")
-                    ActionChains(driver) \
-                        .scroll_to_element(go_to_page) \
-                        .perform()
-                    logger.info("Scrolled to Go to page")
-                    go_to_page.find_element(By.TAG_NAME, "button").click()
-                    items = get_product(driver)
+                    time.sleep(delay)
+                    driver.execute_script(f"window.scrollBy(0, {100 * height});")  # Scrolls down 500px
+                    height += (height + (i * 100))
+                    # Scroll to pagination
+                    go_to_page = driver.find_element(By.CLASS_NAME, ".comet-pagination")
+                    logger.info("Go to page found")
+
+                    if go_to_page:
+                        try:
+                            items = get_product(driver)
+                            logger.error(f"Items on Scroll {items}")
+                            ActionChains(driver) \
+                                .scroll_to_element(go_to_page) \
+                                .perform()
+                            logger.info("Scrolled to Go to page")
+                            go_to_page.find_element(By.TAG_NAME, "button").click()
 
 
-                    # # Wait for the input to appear and click it
-                    # page_input_visible = WebDriverWait(driver, 10).until(
-                    #     EC.element_to_be_clickable(
-                    #         (By.CSS_SELECTOR, '.comet-pagination-options-quick-jumper input[type="text"]'))
-                    # )
-                    # logger.info("dismiss success")
-                    #
-                    # # fill page number
-                    # if page_input_visible:
-                    #     page_input = driver.find_element(By.CSS_SELECTOR,
-                    #                                      '.comet-pagination-options-quick-jumper input[type="text"]')
-                    #     ActionChains(driver) \
-                    #         .scroll_to_element(page_input) \
-                    #         .click() \
-                    #         .send_keys('2') \
-                    #         .perform()
-                    #     logger.info("Type page number success")
-                    # else:
-                    #     logger.info("Type page number failed")
+                            # # Wait for the input to appear and click it
+                            # page_input_visible = WebDriverWait(driver, 10).until(
+                            #     EC.element_to_be_clickable(
+                            #         (By.CSS_SELECTOR, '.comet-pagination-options-quick-jumper input[type="text"]'))
+                            # )
+                            # logger.info("dismiss success")
+                            #
+                            # # fill page number
+                            # if page_input_visible:
+                            #     page_input = driver.find_element(By.CSS_SELECTOR,
+                            #                                      '.comet-pagination-options-quick-jumper input[type="text"]')
+                            #     ActionChains(driver) \
+                            #         .scroll_to_element(page_input) \
+                            #         .click() \
+                            #         .send_keys('2') \
+                            #         .perform()
+                            #     logger.info("Type page number success")
+                            # else:
+                            #     logger.info("Type page number failed")
 
-                    # Wait up to 15 seconds for the button to appear and become clickable
-                    wait = WebDriverWait(driver, 15)
-                    next_button = wait.until(
-                        EC.element_to_be_clickable((By.CSS_SELECTOR, "button.comet-pagination-item-link"))
-                    )
-                    if next_button:
-                        # Click it
-                        next_button.click()
-                        logger.info("Next page button clicked")
-                    else:
-                        return items
+                            # Wait up to 15 seconds for the button to appear and become clickable
+                            wait = WebDriverWait(driver, 15)
+                            next_button = wait.until(
+                                EC.element_to_be_clickable((By.CSS_SELECTOR, "button.comet-pagination-item-link"))
+                            )
+                            if next_button:
+                                # Click it
+                                next_button.click()
+                                logger.info("Next page button clicked")
+                            else:
+                                return items
 
+                        except Exception as e:
+                            logger.error(f"Scroll again : {e}")
                 except Exception as e:
                     logger.error(f"Scroll again : {e}")
-            # finally:
-            #     footer = driver.find_element(By.CSS_SELECTOR, "site-footer")
-            #     if footer:
-            #         logger.info('Page end without pagination')
-            #         return
-        except Exception as e:
-            logger.error(f"Scroll again : {e}")
-
-        logger.info(f"Scrolled {i}")
 
 
 async def main_aliexpress(url, search_text):
